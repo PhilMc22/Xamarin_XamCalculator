d448ee50579abb09eb2e615b252a311c593f77e1
PhilMc22 <pmc2292@gmail.com>
2020-04-08 17:01:12 +0100

    Main page xaml and main page c# updated

--- XamCalculator/XamCalculator/MainPage.xaml.cs
+++ XamCalculator/XamCalculator/MainPage.xaml.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace XamCalculator
+﻿public partial class MainPage : ContentPage
 {
-	// Learn more about making custom code visible in the Xamarin.Forms previewer
-	// by visiting https://aka.ms/xamarinforms-previewer
-	[DesignTimeVisible(false)]
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+
+        AddButton.Clicked += async (s, e) =>
+        {
+            int number1 = 0, number2 = 0;
+
+            var success = int.TryParse(Number1.Text, out number1)
+                && int.TryParse(Number2.Text, out number2);
+
+            if (!success)
+            {
+                await DisplayAlert(
+                    "Error in inputs",
+                    "You must enter two integers", "OK");
+                return;
+            }
+
+            var result = number1 + number2;
+            Result.Text = result + $" {result.GetType()}";
+        };
+    }
 }
